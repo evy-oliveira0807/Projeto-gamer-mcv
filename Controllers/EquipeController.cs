a3f51f61a9cbe7fdb447dfda5d1b6cb31d923951
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-06-02 10:56:03 -0300

    projeto gamer

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -94,6 +94,68 @@ namespace projeto_gamer_manha.Controllers
         }
 
 
+
+        [Route("Editar/{id}")]
+
+        public IActionResult Editar(int id)
+        {
+            Equipe equipe = c.Equipe.First(x => x.IdEquipe == id);
+
+            ViewBag.Equipe = equipe;
+
+            return View("Edit");
+
+        }
+
+        [Route("Atualizar")]
+
+        public IActionResult Atualizar(IFormCollection form)
+        {
+            Equipe equipe = new Equipe();
+
+            equipe.IdEquipe = int.Parse(form["IdEquipe"].ToString());
+
+            equipe.Nome = form["Nome"].ToString();
+
+            if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipe");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(folder, file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                equipe.Imagem = file.FileName;
+            }
+            else
+            {
+                equipe.Imagem = "padrão.png";
+            }
+
+            Equipe equipeBuscada = c.Equipe.First(x => x.IdEquipe == equipe.IdEquipe);
+
+            equipeBuscada.Nome = equipe.Nome;
+
+            equipeBuscada.Imagem = equipe.Imagem;
+
+            c.Equipe.Update(equipeBuscada);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
