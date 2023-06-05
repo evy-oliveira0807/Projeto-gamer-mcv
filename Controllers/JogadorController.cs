567047e433ede53ace06093cc74855ad44572112
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-06-05 08:51:04 -0300

    projeto gamer

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using projeto_gamer_manha.Infra;
+using projeto_gamer_manha.Models;
 
 namespace projeto_gamer_manha.Controllers
 {
@@ -20,12 +21,28 @@ namespace projeto_gamer_manha.Controllers
         }
 
         Context c = new Context();
+
+        [Route("Listar")]
         
         public IActionResult Index()
         {
             ViewBag.Jogador = c.Jogador.ToList();
             ViewBag.Equipe = c.Equipe.ToList();
             return View();
+        }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.Nome = form["Nome"].ToString();
+
+
+            if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
